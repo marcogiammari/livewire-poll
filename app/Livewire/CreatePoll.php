618509ac717c8a714ef6d7e247d67b89d22aacde
Marco Giammari <marcogiammari@gmail.com>
2023-09-30 19:15:56 +0200

    actions in livewire

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,9 +7,22 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = ['First'];
     
     public function render()
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption()
+    {
+        $this->options[] = 'Second';
+    }
+
+    // proprietà semplici si inizializzano come $options
+    // proprietà più complesse si possono inserire nel metodo mount()
+    // public function mount() 
+    // {
+        // es. $options = prendo dati dal db
+    // }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -6,5 +6,19 @@
         <input type="text" wire:model.live="title" />
 
         Current title: {{ $title }}
+
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">
+                Add Option
+            </button>
+        </div>
+
+        <div class="mt-4">
+            @foreach ($options as $index => $option)
+                <div class="mb-4">
+                    {{ $index }} - {{ $option }}
+                </div>
+            @endforeach
+        </div>
     </form>
 </div>
