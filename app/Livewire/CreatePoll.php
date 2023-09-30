6bb711759efa5ed8476daa0895a2e395ec580bbb
Marco Giammari <marcogiammari@gmail.com>
2023-09-30 19:27:22 +0200

    editing poll options

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -19,6 +19,14 @@ class CreatePoll extends Component
         $this->options[] = 'Second';
     }
 
+    public function removeOption($index)
+    {
+        // rimuove l'opzione dall'array
+        unset($this->options[$index]);
+        // ricreiamo l'array per assicurarci che non ci siano "buchi" negli indici
+        $this->options = array_values($this->options);
+    }
+
     // proprietà semplici si inizializzano come $options
     // proprietà più complesse si possono inserire nel metodo mount()
     // public function mount() 
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -16,7 +16,11 @@
         <div class="mt-4">
             @foreach ($options as $index => $option)
                 <div class="mb-4">
-                    {{ $index }} - {{ $option }}
+                    <label>Option {{ $index + 1 }}</label>
+                    <div class="flex gap-2">
+                        <input type="text" wire.model.live="options.{{$index}}">
+                        <button class="btn" wire:click.prevent="removeOption({{$index}})">Remove</button>
+                    </div>
                 </div>
             @endforeach
         </div>
